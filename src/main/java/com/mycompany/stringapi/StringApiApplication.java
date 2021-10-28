94263ead25a3888cad68715d4cec72c2c5b83f2c
I-AzraeL-I <qaraqan70@gmail.com>
2021-10-28 12:17:10 +0200

    first commit

---
+++ src/main/java/com/mycompany/stringapi/StringApiApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.stringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringApiApplication.class, args);
+    }
+
+}
